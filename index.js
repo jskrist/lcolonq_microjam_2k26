af114f61842aed2b8ab4053a9aab8e3e22f8ed16
James Kristoff <jskrist@gmail.com>
2026-05-27 22:25:12 -0400

    update to canvas and javascript memory access

--- index.js
+++ index.js
@@ -1,18 +1,84 @@
-import init, { greet, Universe } from "./pkg/lcolonq_codejam.js";
-
-const pre = document.getElementById("game-of-life-canvas");
-let universe;
-
-const renderLoop = () => {
-    pre.textContent = universe.render();
-    universe.tick();
-    requestAnimationFrame(renderLoop);
-};
+import init, { Universe, Cell } from "./pkg/lcolonq_codejam.js";
 
 // Initialize the wasm module (explicit wasm path) before using any exported
-// functions or types that rely on the `wasm` variable. Create the Universe
-// after initialization and start the render loop.
-init("./pkg/lcolonq_codejam_bg.wasm").then(() => {
-    universe = Universe.new();
-    requestAnimationFrame(renderLoop);
-}).catch(console.error);
+// functions or types that rely on the `wasm` variable.
+const wasm = await init("./pkg/lcolonq_codejam_bg.wasm");
+const memory = wasm.memory;
+
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+// let memory = wasm.memory;
+// Construct the universe, and get its width and height.
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+  universe.tick();
+  drawGrid();
+  drawCells();
+
+  requestAnimationFrame(renderLoop);
+};
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+requestAnimationFrame(renderLoop);
